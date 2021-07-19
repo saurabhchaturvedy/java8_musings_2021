31e21640fc7ea8f3348acbed44e7ab0d34331ade
Saurabh Chaturvedi <56601821+saurabhchaturvedy@users.noreply.github.com>
2021-07-19 19:48:25 +0530

    Update ComparatorLamba.java

--- src/main/java/Introduction_To_Lamba/ComparatorLamba.java
+++ src/main/java/Introduction_To_Lamba/ComparatorLamba.java
@@ -14,5 +14,10 @@ public class ComparatorLambda {
         };
 
         System.out.printf("comparison result is : %d ",comparator.compare(4,5));
+
+        Comparator<Integer> comparator1 = (a,b) -> b.compareTo(a);   // Comparator.naturalOrder && Comparator.reverseOrder()
+
+
+        System.out.printf("comparison result 2 is : %d ",comparator1.compare(6,4));
     }
 }
